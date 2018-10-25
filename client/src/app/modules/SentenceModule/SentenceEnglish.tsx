8c21c80ac4544697245e1304fac33093ab606e43
dottjt <julius.reade@gmail.com>
2018-10-25 13:42:44 +1100

    english sentence options are dandy

--- client/src/app/modules/SentenceModule/SentenceEnglish.tsx
+++ client/src/app/modules/SentenceModule/SentenceEnglish.tsx
@@ -17,17 +17,19 @@ import {
   CONJUGATION_TYPE_VERB_ENGLISH,
 } from '../../../util/constants/optionsConstants';
 
-const wordOptionsEnglish = (word: string, englishSentence: string[], options: Util.Options, index: number): string => {
-  if(startOfSentence(englishSentence.length, index)) {
+const wordOptionsEnglish = (word: string, englishSentenceLength: number, options: Util.Options, index: number, phraseLength: number, phraseIndex: number): string => {
+  if(startOfSentence(englishSentenceLength, index) && startOfSentence(phraseLength, phraseIndex)) {
     return capitalise(word);
   };
-  if (endOfSentence(englishSentence.length, index)) {
+  console.log(phraseLength, phraseIndex);
+
+  if (endOfSentence(englishSentenceLength, index) && endOfSentence(phraseLength, phraseIndex)) {
     return options.question === HAS_QUESTION ? `${word}?` : `${word}.`
   };
   return word;
 };
 
-const phraseOptionsEnglish = (phraseArray: string[], options: Util.Options, index: number): string[] => {
+const phraseOptionsEnglish = (phraseArray: string[], options: Util.Options, phraseIndex: number): string[] => {
   const filteredArray: string[] = phraseArray.filter((word: string): boolean => word !== '');
   return filteredArray;
 };
@@ -53,7 +55,7 @@ class EnglishSentence extends React.Component<PropTypes.IEnglishSentenceProps, {
                 return (
                   <Phrase key={phraseIndex}>
                     {nounPhraseArrayComplete.map((word: string, nounIndex: number) => (
-                      <EnglishWord key={nounIndex}>{wordOptionsEnglish(word, nounPhraseArrayComplete, options, nounIndex)}</EnglishWord>
+                      <EnglishWord key={nounIndex}>{wordOptionsEnglish(word, nounPhraseArrayComplete.length, options, nounIndex, phraseArrayComplete.length, phraseIndex)}</EnglishWord>
                     ))}
                   </Phrase>
                 );
@@ -66,7 +68,7 @@ class EnglishSentence extends React.Component<PropTypes.IEnglishSentenceProps, {
                 return (
                   <Phrase key={phraseIndex}>
                     {verbPhraseArrayComplete.map((word: string, verbIndex: number) => (
-                      <EnglishWord key={verbIndex}>{wordOptionsEnglish(word, verbPhraseArrayComplete, options, verbIndex)}</EnglishWord>
+                      <EnglishWord key={verbIndex}>{wordOptionsEnglish(word, verbPhraseArrayComplete.length, options, verbIndex, phraseArrayComplete.length, phraseIndex)}</EnglishWord>
                     ))};
                   </Phrase>
                 );
