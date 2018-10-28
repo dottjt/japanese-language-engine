ecafd9400d342e5225f6255babf28d9217a54bfd
dottjt <julius.reade@gmail.com>
2018-10-28 16:46:58 +1100

    le graphql, you got your way buddy

--- client/src/graphql/queries.ts
+++ client/src/graphql/queries.ts
@@ -31,15 +31,69 @@ export const GET_ALL_WORDS_AND_OPTIONS = gql`{
     exercises @client {
       englishSentence @client {
         type
-        # tense
-        # polarity
-        # indefiniteArticle
-        noun
-        # verbPolarity
-        # verb
+        tense @client {
+          wordArray
+          wordType
+        }
+        polarity @client {
+          wordArray
+          wordType
+        }
+        indefiniteArticle @client {
+          wordArray
+          wordType
+        }
+        word @client {
+          japanese @client {
+            kanji
+            # hiragana
+            # furigana
+          }
+          english @client {
+            present
+            past
+          }
+          primaryType
+          category
+          meta @client {
+            verbType
+          }
+        }
       }
       japaneseSentence @client {
         type
+        nounCategoryEnding @client {
+          wordArray
+          wordType
+        }
+        nounEnding @client {
+          wordArray
+          wordType
+        }
+        nounTopicParticle @client {
+          wordArray
+          wordType
+        }
+        conjugatedVerb @client {
+          wordArray
+          wordType
+        }
+        word @client {
+          japanese @client {
+            kanji
+            # hiragana
+            # furigana
+          }
+          english @client {
+            present
+            past
+          }
+          primaryType
+          category
+          meta @client {
+            verbType
+          }
+        }
       }
       options @client {
         politeness
