cfeab3eacc5e7443a5fdc288971b5a7a917bf9cc
dottjt <julius.reade@gmail.com>
2018-11-02 08:59:13 +1100

    added api server to folder

---
+++ watashi-api/index.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata';
+
+import { GraphQLServer } from 'graphql-yoga'
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs: './src/schema.graphql', resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))
