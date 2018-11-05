d41d9be6480aedcacaebbf7259e606c74a2d12f8
dottjt <julius.reade@gmail.com>
2018-11-05 14:26:59 +1100

    let us see if this works

--- infrastructure/scripts/deploy.sh
+++ infrastructure/scripts/deploy.sh
@@ -4,9 +4,10 @@ openssl aes-256-cbc -K $encrypted_0aaf8c57bbc0_key -iv $encrypted_0aaf8c57bbc0_i
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
-ssh root@178.128.54.4
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker pull dottjt/watashi-client
-docker run -d -p 80:80 dottjt/watashi-client    
-exit 
+ssh root@178.128.54.4 <<EOF
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker pull dottjt/watashi-client
+  docker run -d -p 80:80 dottjt/watashi-client    
+  exit
+EOF
+echo 'all done!'
