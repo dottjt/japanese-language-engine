4c4e79517dd737648afbd43c0da0652ecb70cd3b
dottjt <julius.reade@gmail.com>
2018-11-08 10:27:02 +1100

    try

--- scripts/deploy_client.sh
+++ scripts/deploy_client.sh
@@ -11,18 +11,29 @@ ssh root@178.128.54.4 <<EOF
   docker stop watashi-client
   docker rm watashi-client
   
-  docker run --name=watashi-client \
-    -p 80:80 -p 443:443 \
-    -v /docker/letsencrypt-docker-nginx/src/production/dh-param/dhparam-2048.pem:/etc/ssl/certs/dhparam-2048.pem \
-    -v /docker-volumes/etc/letsencrypt/live/watashiengine.com/fullchain.pem:/etc/letsencrypt/live/watashiengine.com/fullchain.pem \
-    -v /docker-volumes/etc/letsencrypt/live/watashiengine.com/privkey.pem:/etc/letsencrypt/live/watashiengine.com/privkey.pem \
-    -v /docker-volumes/data/letsencrypt:/data/letsencrypt \
-    -d dottjt/watashi-client
+  docker run --name=watashi-client -d dottjt/watashi-client
+  docker cp watashi-client:/usr/src/app/client/build/. /docker/letsencrypt-docker-nginx/src/production/production-site
+  docker cp watashi-client:/usr/src/app/client/prod.conf /docker/letsencrypt-docker-nginx/src/production/production.conf
+
+  docker stop watashi-client
+  docker rm watashi-client
+
+  cd /docker/letsencrypt-docker-nginx/src/production/
+  docker-compose up -d
 
   exit
 EOF
 
 echo 'all done!'
+
+  # docker run --name=watashi-client \
+  #   -p 80:80 -p 443:443 \
+  #   -v /docker/letsencrypt-docker-nginx/src/production/dh-param/dhparam-2048.pem:/etc/ssl/certs/dhparam-2048.pem \
+  #   -v /docker-volumes/etc/letsencrypt/live/watashiengine.com/fullchain.pem:/etc/letsencrypt/live/watashiengine.com/fullchain.pem \
+  #   -v /docker-volumes/etc/letsencrypt/live/watashiengine.com/privkey.pem:/etc/letsencrypt/live/watashiengine.com/privkey.pem \
+  #   -v /docker-volumes/data/letsencrypt:/data/letsencrypt \
+  #   -d dottjt/watashi-client
+
 
 # docker run --name=watashi-client -d dottjt/watashi-client
 # -v /docker/letsencrypt-docker-nginx/src/production/production-site:/usr/share/nginx/html \
