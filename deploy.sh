e70fc6b37a584b6804bf776efab12937c134fdfd
dottjt <julius.reade@gmail.com>
2018-11-09 14:22:00 +1100

    yoll

--- deploy.sh
+++ deploy.sh
@@ -3,33 +3,30 @@
 mkdir -p $TRAVIS_BUILD_DIR/tmp/build
 docker cp watashi-client:/usr/src/app/client/build/. $TRAVIS_BUILD_DIR/tmp/build
 
-echo 'ls'
-ls
-
-echo 'ls tmp/build'
-ls tmp/build
-
 # build static files
-echo 'rsync tmp/build'
+echo 'echo rsync tmp/build'
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/tmp/build/. root@178.128.54.4:/docker/letsencrypt-docker-nginx/src/production/production-site
 
 # docker-compose.yml file
-echo 'rsync watashi-nginx/docker-compose.yml'
+echo 'echo rsync watashi-nginx/docker-compose.yml'
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/watashi-nginx/docker-compose.yml root@178.128.54.4:/docker/letsencrypt-docker-nginx/src/production
 
 # nginx.conf file
-echo 'rsync watashi-nginx/nginx.conf'
+echo 'echo rsync watashi-nginx/nginx.conf'
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/watashi-nginx/nginx.conf root@178.128.54.4:/docker/letsencrypt-docker-nginx/src/production
 
 ssh root@178.128.54.4 <<EOF
   
-  echo "cd /docker/letsencrypt-docker-nginx/src/production/" 
+  echo "echo cd /docker/letsencrypt-docker-nginx/src/production/" 
   cd /docker/letsencrypt-docker-nginx/src/production/ 
-
-  echo "docker-compose up -d"
+  
+  echo "echo docker-compose down"
+  docker-compose down 
+  
+  echo "echo docker-compose up -d"
   docker-compose up -d 
   
-  echo "exit"
+  echo "echo exit"
   exit
 
 EOF
