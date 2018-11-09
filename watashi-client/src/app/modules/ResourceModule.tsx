b0f0046cdfb0da15cbdb02415e055351c7635f58
dottjt <julius.reade@gmail.com>
2018-11-10 10:28:27 +1100

    try

--- watashi-client/src/app/modules/ResourceModule.tsx
+++ watashi-client/src/app/modules/ResourceModule.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-// import MicrolinkCard from 'react-microlink';
-import { ExternalLink } from '../atoms/ClickableStyles';
+import MicrolinkCard from 'react-microlink';
+// import { ExternalLink } from '../atoms/ClickableStyles';
 import { Flex } from '../atoms/LayoutStyles';
 
 class Resources extends React.Component<PropTypes.IResourcesProps, {}> {
@@ -10,14 +10,17 @@ class Resources extends React.Component<PropTypes.IResourcesProps, {}> {
       <React.Fragment>
         {resources.map((resource: Util.Resource, index: number): any => (
           <Flex mb={2}>
-            {/* <MicrolinkCard 
-              key={index} 
+            <MicrolinkCard 
+              key={index}
+              // screenshot={true}
+              prerender={true}
+              // loading={true}
               url={resource.url} 
               target='_blank' 
-            /> */}
-          <ExternalLink key={index} href={resource.url}>
+            />
+          {/* <ExternalLink key={index} href={resource.url}>
             {resource.url}
-          </ExternalLink>
+          </ExternalLink> */}
 
           </Flex>
         ))}
