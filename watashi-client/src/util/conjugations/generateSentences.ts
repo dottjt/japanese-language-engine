4a6d5470e63cc3eb9c791808a94d7ff392ab3fb8
dottjt <julius.reade@gmail.com>
2018-11-21 19:56:46 +1100

    nipple

--- watashi-client/src/util/conjugations/generateSentences.ts
+++ watashi-client/src/util/conjugations/generateSentences.ts
@@ -31,7 +31,7 @@ import nounConjugationEnglish from './noun/nounConjugationEnglish';
 import prepositionConjugationEnglish from './preposition/prepositionConjugationEnglish';
 
 import verbConjugationEnglish from './verb/verbConjugationEnglish';
-import verbConjugationHelpingEnglish from './verb/verbConjugationHelpingToHaveEnglish';
+import verbConjugationHelpingEnglish from './verb/verbConjugationHelpingEnglish';
 import adverbConjugationEnglish from './adverb/adverbConjugationEnglish';
 import adjectiveConjugationIndefiniteArticleEnglish from './adjective/adjectiveConjugationIndefiniteArticleEnglish';
 
